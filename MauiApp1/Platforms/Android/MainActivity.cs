a8177aa1287363d7e49cad27afdca4075f440aaf
cc <cc@cc.cc>
2025-02-11 21:26:20 +0800

    add

---
+++ MauiApp1/Platforms/Android/MainActivity.cs
@@ -0,0 +1,11 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace MauiApp1
+{
+    [Activity(Theme = "@style/Maui.SplashTheme", ResizeableActivity = true, MainLauncher = true, LaunchMode = LaunchMode.SingleTask)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+    }
+}
